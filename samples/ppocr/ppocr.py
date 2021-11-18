f559239a58f1c40b5f629b69f4d8b084d09314b0
chenenquan <chenenquan0612@hotmail.com>
2021-11-18 16:33:15 +0800

    Add ppocr detection demo in paddle pipeline

--- samples/ppocr/ppocr.py
+++ samples/ppocr/ppocr.py
@@ -8,198 +8,40 @@ from kfp.onprem import use_k8s_secret
 NAMESPACE = "kubeflow"
 
 # model name and file
-MODEL_NAME = "wide-deep"
+MODEL_NAME = "ppocr-det"
 MODEL_VERSION = "latest"
 MODEL_FILE = "wide-deep.tar.gz"
 TRAIN_EPOCH = 4
 
-# SampleSet
-SAMPLE_SET_NAME = "criteo"
-SAMPLE_SET_PATH = "/mnt/criteo"
-
 # data source and storage
 DATA_SOURCE_SECRET_NAME = "data-source"
 DATA_CENTER_SECRET_NAME = "data-center"
-DATA_SOURCE_URI = "bos://paddleflow-public.hkg.bcebos.com/criteo/demo/"
+DATA_SOURCE_URI = "bos://paddleflow-public.hkg.bcebos.com/icdar2015/"
+
+# SampleSet
+SAMPLE_SET_NAME = "icdar2015"
+SAMPLE_SET_PATH = "/mnt/icdar2015"
 
 # config file and path
 CONFIG_PATH = "/mnt/config/"
-CONFIG_FILE = "wide_deep_config.yaml"
+CONFIG_FILE = "det_mv3_db.yml"
 
 # PaddleJob
 TASK_MOUNT_PATH = "/mnt/task-center/"
-MODEL_CHECKPOINT_PATH = "/mnt/task-center/models/"
+MODEL_PATH = "/mnt/task-center/models/"
+PADDLE_JOB_IMAGE = "registry.baidubce.com/paddleflow-public/paddleocr:2.1.0-gpu-cuda10.2-cudnn7"
 
 # minio host
 MINIO_ENDPOINT = "http://minio-service.kubeflow:9000"
 
-# images
-PADDLE_JOB_IMAGE = "registry.baidubce.com/paddleflow-public/paddlerec:2.1.0-gpu-cuda10.2-cudnn7"
+# Serving Image
 SERVING_IMAGE = "registry.baidubce.com/paddleflow-public/serving"
-
-
-def create_sample_set():
-    sampleset_launcher_op = components.load_component_from_file("../../components/sampleset.yaml")
-
-    return sampleset_launcher_op(
-        name=SAMPLE_SET_NAME,
-        namespace=NAMESPACE,
-        action="apply",
-        partitions=1,
-        secret_ref={"name": DATA_CENTER_SECRET_NAME}
-    ).set_display_name(f"create sample set {SAMPLE_SET_NAME}")
-
-
-def create_sample_job():
-    samplejob_launcher_op = components.load_component_from_file("../../components/samplejob.yaml")
-
-    sync_options = {
-        "syncOptions": {
-            "source": DATA_SOURCE_URI,
-            "destination": SAMPLE_SET_NAME,
-        }
-    }
-    return samplejob_launcher_op(
-        name="criteo-sync",
-        namespace=NAMESPACE,
-        type="sync",
-        delete_after_done=True,
-        job_options=sync_options,
-        sampleset_ref={"name": SAMPLE_SET_NAME},
-        secret_ref={"name": DATA_SOURCE_SECRET_NAME}
-    ).set_display_name("sync remote data to local")
-
-
-def write_config_file(path: str, filename: str, content: str):
-    import os
-    if not os.path.exists(path):
-        os.makedirs(path)
-    filepath = os.path.join(path, filename)
-    with open(filepath, "w") as f:
-        f.write(content)
-
-    print("write config success: \n\n {}".format(content))
-
-
-def create_model_config(volume_op):
-    wide_deep_config = f"""
-# wide_deep_config.yaml
-# global settings
-runner:
-  train_data_dir: "{SAMPLE_SET_PATH}/{SAMPLE_SET_NAME}/slot_train_data_full/"
-  train_reader_path: "criteo_reader" # importlib format
-  use_gpu: True
-  use_auc: True
-  train_batch_size: 4096
-  epochs: {TRAIN_EPOCH}
-  print_interval: 10
-  model_save_path: {MODEL_CHECKPOINT_PATH}
-  test_data_dir: "{SAMPLE_SET_PATH}/{SAMPLE_SET_NAME}/slot_test_data_full"
-  infer_reader_path: "criteo_reader" # importlib format
-  infer_batch_size: 4096
-  infer_load_path: {MODEL_CHECKPOINT_PATH}
-  infer_start_epoch: 0
-  infer_end_epoch: 4
-  use_inference: True
-  save_inference_feed_varnames: ["C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","C11","C12","C13","C14","C15","C16","C17","C18","C19","C20","C21","C22","C23","C24","C25","C26","dense_input"]
-  save_inference_fetch_varnames: ["sigmoid_0.tmp_0"]
-  #use fleet
-  use_fleet: True
-
-# hyper parameters of user-defined network
-hyper_parameters:
-  # optimizer config
-  optimizer:
-    class: Adam
-    learning_rate: 0.001
-    strategy: async
-  # user-defined <key, value> pairs
-  sparse_inputs_slots: 27
-  sparse_feature_number: 1000001
-  sparse_feature_dim: 9
-  dense_input_dim: 13
-  fc_sizes: [512, 256, 128, 32]
-  distributed_embedding: 0
-"""
-
-    create_config = components.create_component_from_func(
-        func=write_config_file,
-        base_image="python:3.7",
-    )
-    task_op = create_config(CONFIG_PATH, CONFIG_FILE, wide_deep_config)
-    task_op.add_pvolumes({CONFIG_PATH: volume_op.volume})
-    task_op.set_display_name("create config file")
-
-    return task_op
-
-
-def create_paddle_job(volume_op):
-    paddlejob_launcher_op = components.load_component_from_file("../../components/paddlejob.yaml")
-    args = f"cp {os.path.join(TASK_MOUNT_PATH, CONFIG_FILE)} . &&  " \
-           f"python -m paddle.distributed.launch --log_dir {TASK_MOUNT_PATH} ../../../tools/static_trainer.py -m {CONFIG_FILE}"
-
-    container = {
-        "name": "paddlerec",
-        "image": PADDLE_JOB_IMAGE,
-        "workingDir": "/home/PaddleRec/models/rank/wide_deep",
-        "command": ["/bin/bash"],
-        "args": [
-            "-c", args
-        ],
-        "volumeMounts": [
-            {
-                "name": "dshm",
-                "mountPath": "/dev/shm"
-            },
-            {
-                "name": "task-volume",
-                "mountPath": TASK_MOUNT_PATH,
-            }
-        ],
-        "resources": {
-            "limits": {
-                "nvidia.com/gpu": 1
-            }
-        }
-    }
-
-    worker = {
-        "replicas": 1,
-        "template": {"spec": {
-            "containers": [container],
-            "volumes": [
-                {
-                    "name": "dshm",
-                    "emptyDir": {
-                        "medium": "Memory"
-                    },
-                },
-                {
-                    "name": "task-volume",
-                    "persistentVolumeClaim": {
-                        "claimName": volume_op.volume.persistent_volume_claim.claim_name
-                    }
-                }
-            ]},
-        }
-    }
-
-    return paddlejob_launcher_op(
-        name="wide-and-deep",
-        namespace=NAMESPACE,
-        delete_after_done=True,
-        worker_spec=worker,
-        sampleset_ref={
-            "name": SAMPLE_SET_NAME,
-            "mountPath": SAMPLE_SET_PATH,
-        }
-    ).set_display_name("train wide and deep")
 
 
 def create_volume_op():
     return dsl.VolumeOp(
-        name="Wide Deep PVC",
-        resource_name="wide-deep-pvc",
+        name="PPOCR Detection PVC",
+        resource_name="ppocr-detection-pvc",
         storage_class="task-center",
         size="10Gi",
         modes=dsl.VOLUME_MODE_RWM
@@ -229,19 +71,247 @@ def create_resource_op():
     ).add_pod_annotation(name="pipelines.kubeflow.org/max_cache_staleness", value="P0D")
 
 
-def create_convert_op(volume_op):
-    args = f"cd {TASK_MOUNT_PATH} && mkdir -p {MODEL_NAME} && " \
-           f"python3 -m paddle_serving_client.convert --dirname {MODEL_CHECKPOINT_PATH}{TRAIN_EPOCH-1}/ " \
-           f"--model_filename rec_inference.pdmodel --params_filename rec_inference.pdiparams " \
-           f"--serving_server ./{MODEL_NAME}/server/ --serving_client ./{MODEL_NAME}/client/ &&" \
-           f"tar czf {MODEL_NAME}.tar.gz {MODEL_NAME}/"
+def create_sample_set():
+    sampleset_launcher_op = components.load_component_from_file("../../components/sampleset.yaml")
 
-    convert_op = dsl.ContainerOp(
-        name="Convert wide&Deep Model",
-        image=f"{SERVING_IMAGE}:v0.6.2",
-        command="/bin/bash",
-        arguments=["-c", args],
-    )
+    return sampleset_launcher_op(
+        name=SAMPLE_SET_NAME,
+        namespace=NAMESPACE,
+        action="apply",
+        partitions=1,
+        secret_ref={"name": DATA_CENTER_SECRET_NAME}
+    ).set_display_name(f"create sample set {SAMPLE_SET_NAME}")
+
+
+def create_model_config(volume_op):
+    det_mv3_db = f"""
+Global:
+  use_gpu: true
+  epoch_num: 1200
+  log_smooth_window: 20
+  print_batch_step: 10
+  save_model_dir: {MODEL_PATH}
+  save_epoch_step: 1200
+  # evaluation is run every 2000 iterations after the 0th iteration
+  eval_batch_step: [0, 2000]
+  cal_metric_during_train: False
+  pretrained_model: ./pretrain_models/MobileNetV3_large_x0_5_pretrained
+  checkpoints:
+  save_inference_dir:
+  use_visualdl: False
+  infer_img: doc/imgs_en/img_10.jpg
+  save_res_path: ./output/det_db/predicts_db.txt
+
+Architecture:
+  model_type: det
+  algorithm: DB
+  Transform:
+  Backbone:
+    name: MobileNetV3
+    scale: 0.5
+    model_name: large
+  Neck:
+    name: DBFPN
+    out_channels: 256
+  Head:
+    name: DBHead
+    k: 50
+
+Loss:
+  name: DBLoss
+  balance_loss: true
+  main_loss_type: DiceLoss
+  alpha: 5
+  beta: 10
+  ohem_ratio: 3
+
+Optimizer:
+  name: Adam
+  beta1: 0.9
+  beta2: 0.999
+  lr:
+    learning_rate: 0.001
+  regularizer:
+    name: 'L2'
+    factor: 0
+
+PostProcess:
+  name: DBPostProcess
+  thresh: 0.3
+  box_thresh: 0.6
+  max_candidates: 1000
+  unclip_ratio: 1.5
+
+Metric:
+  name: DetMetric
+  main_indicator: hmean
+
+Train:
+  dataset:
+    name: SimpleDataSet
+    data_dir: {SAMPLE_SET_PATH}/{SAMPLE_SET_NAME}/icdar_c4_train_imgs/
+    label_file_list:
+      - {SAMPLE_SET_PATH}/{SAMPLE_SET_NAME}/train_icdar2015_label.txt
+    ratio_list: [1.0]
+    transforms:
+      - DecodeImage: # load image
+          img_mode: BGR
+          channel_first: False
+      - DetLabelEncode: # Class handling label
+      - IaaAugment:
+          augmenter_args:
+            - { 'type': Fliplr, 'args': { 'p': 0.5 } }
+            - { 'type': Affine, 'args': { 'rotate': [-10, 10] } }
+            - { 'type': Resize, 'args': { 'size': [0.5, 3] } }
+      - EastRandomCropData:
+          size: [640, 640]
+          max_tries: 50
+          keep_ratio: true
+      - MakeBorderMap:
+          shrink_ratio: 0.4
+          thresh_min: 0.3
+          thresh_max: 0.7
+      - MakeShrinkMap:
+          shrink_ratio: 0.4
+          min_text_size: 8
+      - NormalizeImage:
+          scale: 1./255.
+          mean: [0.485, 0.456, 0.406]
+          std: [0.229, 0.224, 0.225]
+          order: 'hwc'
+      - ToCHWImage:
+      - KeepKeys:
+          keep_keys: ['image', 'threshold_map', 'threshold_mask', 'shrink_map', 'shrink_mask'] # the order of the dataloader list
+  loader:
+    shuffle: True
+    drop_last: False
+    batch_size_per_card: 16
+    num_workers: 8
+    use_shared_memory: False
+
+Eval:
+  dataset:
+    name: SimpleDataSet
+    data_dir: {SAMPLE_SET_PATH}/{SAMPLE_SET_NAME}/ch4_test_images/
+    label_file_list:
+      - {SAMPLE_SET_PATH}/{SAMPLE_SET_NAME}/test_icdar2015_label.txt
+    transforms:
+      - DecodeImage: # load image
+          img_mode: BGR
+          channel_first: False
+      - DetLabelEncode: # Class handling label
+      - DetResizeForTest:
+          image_shape: [736, 1280]
+      - NormalizeImage:
+          scale: 1./255.
+          mean: [0.485, 0.456, 0.406]
+          std: [0.229, 0.224, 0.225]
+          order: 'hwc'
+      - ToCHWImage:
+      - KeepKeys:
+          keep_keys: ['image', 'shape', 'polys', 'ignore_tags']
+  loader:
+    shuffle: False
+    drop_last: False
+    batch_size_per_card: 1 # must be 1
+    num_workers: 8
+    use_shared_memory: False
+"""
+    write_config = components.load_component_from_file("../../components/configure.yaml")
+    write_config_op = write_config(path=CONFIG_PATH, filename=CONFIG_FILE, content=det_mv3_db)
+
+    write_config_op.add_pvolumes({CONFIG_PATH: volume_op.volume})
+    write_config_op.set_display_name("create config file")
+    return write_config_op
+
+
+def create_sample_job():
+    samplejob_launcher_op = components.load_component_from_file("../../components/samplejob.yaml")
+
+    sync_options = {
+        "syncOptions": {
+            "source": DATA_SOURCE_URI,
+            "destination": SAMPLE_SET_NAME,
+        }
+    }
+    return samplejob_launcher_op(
+        name="icdar2015-sync",
+        namespace=NAMESPACE,
+        type="sync",
+        delete_after_done=True,
+        job_options=sync_options,
+        sampleset_ref={"name": SAMPLE_SET_NAME},
+        secret_ref={"name": DATA_SOURCE_SECRET_NAME}
+    ).set_display_name("sync remote data to local")
+
+
+def create_paddle_job(volume_op):
+    paddlejob_launcher_op = components.load_component_from_file("../../components/paddlejob.yaml")
+
+    container = {
+        "name": "paddleocr",
+        "image": PADDLE_JOB_IMAGE,
+        "workingDir": "/home/PaddleRec/models/rank/wide_deep",
+        "command": ["python3"],
+        "args": [
+            "-m", "paddle.distributed.launch",
+            "--gpus", "0,1",
+            "tools/train.py",
+            "-c", f"{CONFIG_PATH}/{CONFIG_FILE}",
+        ],
+        "volumeMounts": [
+            {
+                "name": "dshm",
+                "mountPath": "/dev/shm"
+            },
+            {
+                "name": "task-volume",
+                "mountPath": TASK_MOUNT_PATH,
+            }
+        ],
+        "resources": {
+            "limits": {
+                "nvidia.com/gpu": 2
+            }
+        }
+    }
+
+    worker = {
+        "replicas": 1,
+        "template": {"spec": {
+            "containers": [container],
+            "volumes": [
+                {
+                    "name": "dshm",
+                    "emptyDir": {
+                        "medium": "Memory"
+                    },
+                },
+                {
+                    "name": "task-volume",
+                    "persistentVolumeClaim": {
+                        "claimName": volume_op.volume.persistent_volume_claim.claim_name
+                    }
+                }
+            ]},
+        }
+    }
+
+    return paddlejob_launcher_op(
+        name="ppocr-det",
+        namespace=NAMESPACE,
+        delete_after_done=True,
+        worker_spec=worker,
+        sampleset_ref={
+            "name": SAMPLE_SET_NAME,
+            "mountPath": SAMPLE_SET_PATH,
+        }
+    ).set_display_name("train ppocr detection model")
+
+
+def create_convert_op(volume_op):
+    model_converter = components.load_component_from_file("../../components/paddlejob.yaml")
+    convert_op = model_converter(mount_path=TASK_MOUNT_PATH, model_name=MODEL_NAME, dirname=MODEL_PATH)
     convert_op.add_pvolumes({TASK_MOUNT_PATH: volume_op.volume})
     convert_op.set_display_name("Convert Model Format")
     return convert_op
@@ -286,33 +356,32 @@ def create_serving_op():
     }
 
     serving_op = create_serving(
-        name="wide-deep-serving",
+        name="ppocr-det-serving",
         namespace="kubeflow",
         action="apply",
         default=default,
         runtime_version="paddleserving",
         service={"minScale": 1}
     )
-    serving_op.set_display_name("deploy wide-deep serving")
+    serving_op.set_display_name("deploy ppocr det serving")
     return serving_op
 
 
 @dsl.pipeline(
-    name="paddle-wide-deep",
-    description="An example to train wide-deep-deep using paddle.",
+    name="ppocr-detection-demo",
+    description="An example for using ppocr train .",
 )
-def wide_deep_demo():
+def ppocr_detection_demo():
     # 1. create volume for config task and PaddleJob
     volume_op = create_volume_op()
 
     # 1. create secret for data source
     secret_op = create_resource_op()
 
-    # 2. create or update SampleSet for Criteo which is stored in remote storage
+    # 2. create or update SampleSet
     sample_set_task = create_sample_set()
-    # sample_set_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
 
-    # 2. create configmap for wide-and-deep model
+    # 2. create configmap for model
     create_config_task = create_model_config(volume_op)
     create_config_task.after(volume_op)
     create_config_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
@@ -343,21 +412,19 @@ def wide_deep_demo():
     serving_op.after(upload_op)
     serving_op.execution_options.caching_strategy.max_cache_staleness = "P0D"
 
-    # 8. get predict ctr from wide & deep model server
-
 
 if __name__ == "__main__":
     import kfp.compiler as compiler
 
-    pipeline_file = "wide_deep_demo.yaml"
+    pipeline_file = "ppocr_detection_demo.yaml"
 
-    compiler.Compiler().compile(wide_deep_demo, pipeline_file)
+    compiler.Compiler().compile(ppocr_detection_demo, pipeline_file)
 
     client = kfp.Client(host="http://www.my-pipeline-ui.com:80")
     run = client.create_run_from_pipeline_package(
         pipeline_file,
         arguments={},
-        run_name="paddle wide-and-deep demo",
+        run_name="paddle ocr detection demo",
         service_account="pipeline-runner"
     )
     print(f"Created run {run}")
